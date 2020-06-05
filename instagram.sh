4d50417d93a28efe22285a169350db4d13b4a850
Ryankusumabagas <64182129+Ryankusumabagas@users.noreply.github.com>
2020-06-05 09:18:05 +0700

    Jagilinsta.sh

--- instagram.sh
+++ instagram.sh
@@ -39,5 +39,5 @@ echo $HUJAU"Apakah username $target sudah benar?"
 echo y/n?
 read confirm
 echo
-cek_target=`curl -s http://zpro.000webhostapp.com/api/cek_list_ig.php?cari=$target`
+echo cek_target=`curl -s http://zpro.000webhostapp.com/api/cek_list_ig.php?cari=$target`
 echo $MERAH$cek_target$N
